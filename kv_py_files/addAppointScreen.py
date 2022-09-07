326147ae79636e4476f2376a66e760f7a6ddb0a5
Kepins <sz.kepinski1@gmail.com>
2022-09-07 12:27:56 +0200

    AddAppointScreen in development

--- kv_py_files/addAppointScreen.py
+++ kv_py_files/addAppointScreen.py
@@ -1,10 +1,13 @@
 import datetime
 
+from kivy.properties import StringProperty
 from kivy.uix.screenmanager import Screen
 
 
 class AddAppointScreen(Screen):
     day: datetime.date
+    day_str = StringProperty()
 
     def on_pre_enter(self, *args):
+        self.day_str = self.day.strftime("%m/%d")
         print(self.day)
